c2e7f36e6ff4cf57d808da3b1e6e04de795b41d3
Ayen Mejorada <mejoradaayen1102@gmail.com>
2025-04-06 16:01:36 +0800

    Add files via upload

---
+++ JS-4/script.js
@@ -0,0 +1,17 @@
+for (let i = 1; i <= 10; i++) {
+    console.log(`Number: ${i}`);
+}
+
+let hobbies = [
+    "Running",
+    "Drawing",
+    "Designing",
+    "Gaming",
+    "Studying"
+];
+
+let index = 0;
+while (index < hobbies.length) {
+    console.log(`Hobby ${index + 1}: ${hobbies[index]}`);
+    index++;
+}
